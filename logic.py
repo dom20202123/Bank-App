f1eea791923431ea2443bc00251614a757b4697e
dom20202123 <dominiquecurry2006@gmail.com>
2025-05-09 19:54:57 -0500

    added file

--- logic.py
+++ logic.py
@@ -4,7 +4,11 @@ import csv
 
 
 class Logic(QMainWindow, Ui_Bank_App):
-    def __init__(self):
+    def __init__(self) -> None:
+        '''
+        Sets up the screen and hiding majority of inputs until the login is complete.
+        Also connects the buttons to their functions
+        '''
         super().__init__()
         self.setupUi(self)
         self.Welcome_Label.hide()
@@ -20,7 +24,14 @@ class Logic(QMainWindow, Ui_Bank_App):
         self.Submit_Button.clicked.connect(lambda : self.transfer())
 
 
-    def account_check(self):
+    def account_check(self) -> None:
+        '''
+        This function covers over the login button which allows the user to access their file information in info.csv
+        first : String value that holds the first name of the user
+        second : String value that holds the last name of the user
+        password : holds the pin for the user account which needs to match for the user to login in
+        user_found : value that's True or False that confirms if the user account was found or not
+        '''
         self.first = self.First_Name_Input.text()
         self.last = self.Last_Name_Input.text()
         password = self.Pin_Input.text()
@@ -65,7 +76,18 @@ class Logic(QMainWindow, Ui_Bank_App):
                     self.Amount_Input.show()
                     self.Balance_input.setText(f'$0.00')
 
-    def transfer(self):
+    def transfer(self) -> None:
+        '''
+        The function that controls over the transfer of money to accounts which allows you to eithr deposit or withdraw
+        The change in function is then submitted to info.csv which holds account infomation
+        first : String value that holds the first name of the user
+        second : String value that holds the last name of the user
+        num : String value of the current balance
+        rows : List created to hold the rows in info so it could be changed
+        balance: a float value of the account balance
+        amount : a float value of the amount being changed
+
+        '''
         first = self.first
         last = self.last
         num = self.Amount_Input.text()
